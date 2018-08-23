e86e601c2ea1b7928cd109d2acc360bf90b3693f
金正朋也 <mhtnim1023@gmail.com>
2018-08-23 22:00:47 +0900

    アクション名ごとに処理を分ける

--- routes/index.js
+++ routes/index.js
@@ -11,19 +11,21 @@ router.post('/webhook', function(req, res, next) {
     const line_sender = new LineSender();
     if (event.message.text == "教えて") {
       line_sender.pushQuickReply(line_sender.make_quick_reply_json());
-    } else if (event.message.text == '今日すること') {
-      return
     } else {
       line_sender.pushMessage(line_sender.make_message_json(['何言ってるの？？']));
     }
   } else if (event.postback) {
     var trello = new Trello();
+    var action_name;
     if (event.postback.data.match('to_do_today')) {
       // 今日することを取得
-      trello.get_and_push_trello_cards();
+      action_name = event.postback.data.match('to_do_today')[0]
+      trello.get_and_push_trello_cards(action_name);
     } else if (event.postback.data.match('to_do_tomorrow')) {
       // 明日することを取得
-      
+      action_name = event.postback.data.match('to_do_tomorrow')[0]
+      trello.get_and_push_trello_cards(action_name);
+
     }
   }
 });
