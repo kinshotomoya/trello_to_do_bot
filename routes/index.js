3ef945e2e1583404901a7e35ca498e980ce6fcd5
金正朋也 <mhtnim1023@gmail.com>
2018-08-23 21:43:13 +0900

    返事を追加

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,23 @@ const LineSender = require('../node_api_request');
 router.post('/webhook', function(req, res, next) {
   var event = req.body.events[0]
   if (event.message) {
+    const line_sender = new LineSender();
     if (event.message.text == "教えて") {
-      const line_sender = new LineSender();
       line_sender.pushQuickReply(line_sender.make_quick_reply_json());
     } else if (event.message.text == '今日すること') {
       return
     } else {
-      console.log('回答できません');
+      line_sender.pushMessage(line_sender.make_message_json(['何言ってるの？？']));
     }
   } else if (event.postback) {
     var trello = new Trello();
-    trello.get_and_push_trello_cards();
+    if (event.postback.data.match('to_do_today')) {
+      // 今日することを取得
+      trello.get_and_push_trello_cards();
+    } else if (event.postback.data.match('to_do_tomorrow')) {
+      // 明日することを取得
+      
+    }
   }
 });
 
